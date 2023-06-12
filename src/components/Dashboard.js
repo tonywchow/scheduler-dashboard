055d73dee41657016c463e9c5969bcdda0c0a919
Tony Chow <tchow9@gmail.com>
2023-06-12 06:41:09 -0700

    Add fetching data from api server and lifecycle methods

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,43 +1,81 @@
 import React, { Component } from "react";
-
+import axios from "axios";
 import classnames from "classnames";
 
 import Loading from "./Loading";
 import Panel from "./Panel";
 
+import {
+  getTotalInterviews,
+  getLeastPopularTimeSlot,
+  getMostPopularDay,
+  getInterviewsPerDay,
+} from "helpers/selectors";
+
 const data = [
   {
     id: 1,
     label: "Total Interviews",
-    value: 6,
+    getValue: getTotalInterviews,
   },
   {
     id: 2,
     label: "Least Popular Time Slot",
-    value: "1pm",
+    getValue: getLeastPopularTimeSlot,
   },
   {
     id: 3,
     label: "Most Popular Day",
-    value: "Wednesday",
+    getValue: getMostPopularDay,
   },
   {
     id: 4,
     label: "Interviews Per Day",
-    value: "2.3",
+    getValue: getInterviewsPerDay,
   },
 ];
 
 class Dashboard extends Component {
   state = {
-    loading: false,
+    loading: true,
     focused: null,
+    days: [],
+    appointments: {},
+    inteviewer: {},
   };
   // Instance Method
   selectPanel(id) {
     this.setState((previousState) => ({
       focused: previousState.focused !== null ? null : id,
     }));
+  }
+  //Lifecycle method to check to see if there is a saved focus state after we render the application the first time
+  componentDidMount() {
+    const focused = JSON.parse(localStorage.getItem("focused"));
+
+    Promise.all([
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers"),
+    ]).then(([days, appointments, interviewers]) => {
+      this.setState({
+        loading: false,
+        days: days.data,
+        appointments: appointments.data,
+        interviewers: interviewers.data,
+      });
+    });
+
+    if (focused) {
+      this.setState({ focused });
+    }
+  }
+  //Lifecycle method to listen for changes to the state
+  //comparing the previous update to the existing state, and if the values change, we write the value to localStorage
+  componentDidUpdate(previousPros, previousState) {
+    if (previousState.focused !== this.state.focused) {
+      localStorage.setItem("focused", JSON.stringify(this.state.focused));
+    }
   }
 
   render() {
@@ -59,7 +97,7 @@ class Dashboard extends Component {
           key={panel.id}
           id={panel.id}
           label={panel.label}
-          value={panel.label}
+          value={panel.getValue(this.state)}
           onSelect={(event) => this.selectPanel(panel.id)}
         />
       );
